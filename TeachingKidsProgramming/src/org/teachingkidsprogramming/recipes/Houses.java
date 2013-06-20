dabe55992dee29ebb5817f8b5599bb98ffb4fec4
MIHIR-SHAH <MIHIR@unknown0026bb039016.gateway.2wire.net>
2013-06-19 18:58:49 -0700

    More customizable!-Mihir

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
@@ -9,19 +9,24 @@ public class Houses
 {
   public static void main(String[] args)
   {
-    Tortoise.setSpeed(10);
+    Tortoise.setSpeed(9);
     Tortoise.setX(200);
-    int height = 40;
-    drawHouse(height);
-    drawHouse(120);
-    drawHouse(90);
-    drawHouse(20);
-    drawHouse(78);
-    drawHouse(100);
+    int height1 = MessageBox.askForNumericalInput("How tall would you like your house?");
+    drawHouse(height1);
+    int height2 = MessageBox.askForNumericalInput("How tall would you like your house?");
+    drawHouse(height2);
+    int height3 = MessageBox.askForNumericalInput("How tall would you like your house?");
+    drawHouse(height3);
+    int height4 = MessageBox.askForNumericalInput("How tall would you like your house?");
+    drawHouse(height4);
+    int height5 = MessageBox.askForNumericalInput("How tall would you like your house?");
+    drawHouse(height5);
+    int height6 = MessageBox.askForNumericalInput("How tall would you like your house?");
+    drawHouse(height6);
   }
   private static void drawHouse(int height)
   {
-    int roofWidth = MessageBox.askForNumericalInput("How wide do you want the roof to be?");
+    int roofWidth = MessageBox.askForNumericalInput("What roof side length do you want?");
     Tortoise.setPenWidth(3);
     Tortoise.setPenColor(Color.black);
     Tortoise.move(height);
