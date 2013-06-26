32f807bec45452d85babc3f61cc5d078d5907a18
Mihir Shah <mihirkrishi@sbcglobal.net>
2013-06-26 14:00:10 -0700

    xcommit--matt/mihir

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/AdLibs.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/AdLibs.java
@@ -1,17 +1,22 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.windows.MessageBox;
 
 public class AdLibs
 {
   public static void main(String[] args)
   {
-    //Ask the user to enter an adverb, save it as currentAdverb --#2
+    String adverb = MessageBox.askForTextInput("Enter an adverb.");
     //Ask the user to enter a verb ending in '-ed', save it as currentEdVerb --#4
+    String edVerb = MessageBox.askForTextInput("Enter a verb ending in '-ed.'");
     //Ask the user to enter a body part, save it as currentBodyPart --#6
-    //Set the value of the currentStory to the word "Today " --#1.2
-    //Add the words "I woke " + currentAdverb + ". " to the currentStory --#3
+    String bodyPart = MessageBox.askForTextInput("Enter a body part.");
+    String story = "Today ";
+    story += "I woke " + adverb + ". ";
     //Add the words '"Then I " + currentEdVerb + " " to the currentStory --#5
+    story += "Then I " + edVerb + " ";
     //Add the words "my " + currentBodyPart + ". " to the current story --#7
-    //Show the currentStory in a message box as a message --#1.1
+    story += "my " + bodyPart + ". ";
+    MessageBox.showMessage(story);
   }
 }
