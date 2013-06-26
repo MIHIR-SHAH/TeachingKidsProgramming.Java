143d6701ab723a23ac73677ebd1f62c93dd86227
Mihir Shah <mihirkrishi@sbcglobal.net>
2013-06-26 14:41:28 -0700

    i dyed my epidermis.-mihir/matt

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/AdLibs.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/AdLibs.java
@@ -1,22 +1,19 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.simpleparser.Parser;
+import org.teachingextensions.utils.Viewer;
 import org.teachingextensions.windows.MessageBox;
 
 public class AdLibs
 {
   public static void main(String[] args)
   {
-    String adverb = MessageBox.askForTextInput("Enter an adverb.");
-    //Ask the user to enter a verb ending in '-ed', save it as currentEdVerb --#4
-    String edVerb = MessageBox.askForTextInput("Enter a verb ending in '-ed.'");
-    //Ask the user to enter a body part, save it as currentBodyPart --#6
-    String bodyPart = MessageBox.askForTextInput("Enter a body part.");
-    String story = "Today ";
-    story += "I woke " + adverb + ". ";
-    //Add the words '"Then I " + currentEdVerb + " " to the currentStory --#5
-    story += "Then I " + edVerb + " ";
-    //Add the words "my " + currentBodyPart + ". " to the current story --#7
-    story += "my " + bodyPart + ". ";
-    MessageBox.showMessage(story);
+    Words words = new Words();
+    words.adverb = MessageBox.askForTextInput("Enter an adverb.");
+    words.edVerb = MessageBox.askForTextInput("Enter a verb ending in '-ed.'");
+    words.bodyPart = MessageBox.askForTextInput("Enter a body part.");
+    String story = Parser.parseRtfFile("view.rtf", words);
+    //MessageBox.showMessage(story);
+    Viewer.displayRtfFile(story);
   }
 }
