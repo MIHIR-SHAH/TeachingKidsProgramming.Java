369d1bdc060b9dd985837c0ca13d306c94aeddb1
Mihir Shah <mihirkrishi@sbcglobal.net>
2013-06-26 14:59:00 -0700

    i cut my legs-mihir/matt

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/AdLibs.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/AdLibs.java
@@ -10,8 +10,8 @@ public class AdLibs
   {
     Words words = new Words();
     words.adverb = MessageBox.askForTextInput("Enter an adverb.");
-    words.edVerb = MessageBox.askForTextInput("Enter a verb ending in '-ed.'");
-    words.bodyPart = MessageBox.askForTextInput("Enter a body part.");
+    words.edVerb = MessageBox.askForTextInput("Enter a past-tense verb");
+    words.bodyPart = MessageBox.askForTextInput("Enter a noun that you own.");
     String story = Parser.parseRtfFile("view.rtf", words);
     //MessageBox.showMessage(story);
     Viewer.displayRtfFile(story);
